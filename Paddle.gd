265b8bd82e1d8ff4b929ad70190a908080217e49
thejacer87 <jacebennest87@gmail.com>
2022-11-25 22:00:31 -0800

    fix animation

--- Paddle.gd
+++ Paddle.gd
@@ -5,19 +5,20 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var score = 0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
+	var rotate = Input.get_axis("paddle_rotate_up", "paddle_rotate_down")
+	rotation = deg_to_rad(15.0) * rotate
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("paddle_up", "paddle_down")
 	velocity.y = direction * SPEED
 
 	move_and_slide()
+	pass
 
+func _input(event):
+	if event.is_action_pressed("smack"):
+		$AnimationPlayer.play("Smack")
 
 func add_points(points = 1):
 	score += points
